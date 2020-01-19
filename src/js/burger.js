c5fb36772b65fda7fe7f8f8ab129ba89819f2c67
Анастасия <eugengamilov@gmail.com>
2020-01-19 12:46:01 +0200

    JS_add

---
+++ src/js/burger.js
@@ -0,0 +1,31 @@
+'use strict';
+const burgerBtn = document.querySelector('.mobile-navigation-btn');
+const modalMenu = document.querySelector('.mobile-site-navigation');
+const modalAnchor = [...document.querySelectorAll('.site-navigation__link')];
+
+let openModal = (e) => {
+    let target = e.target;
+    
+    if (modalMenu.classList.contains('mobile-site-navigation_active')) {
+        burgerBtn.style.backgroundImage = "url('../images/icon-burger.png')";
+        modalMenu.classList.toggle('mobile-site-navigation_active');
+    } else {
+        modalMenu.classList.toggle('mobile-site-navigation_active');
+        burgerBtn.style.backgroundImage = "url('../images/icon-burger-closed.png')";
+    }
+}
+
+let closeModal = () => {
+    if (modalMenu.classList.contains('mobile-site-navigation_active')) {
+        burgerBtn.style.backgroundImage = "url('../images/icon-burger.png')";
+        modalMenu.classList.toggle('mobile-site-navigation_active');
+    } else {
+        modalMenu.classList.toggle('mobile-site-navigation_active');
+        burgerBtn.style.backgroundImage = "url('../images/icon-burger-closed.png')";
+    }
+}
+
+modalAnchor.forEach(e => {
+    e.addEventListener('click', closeModal);
+})
+burgerBtn.addEventListener('click', openModal);
